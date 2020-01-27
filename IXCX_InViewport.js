a4701607830beefc30db6366738bf5b5e8e1ab5d
indexcodex <60354991+indexcodex@users.noreply.github.com>
2020-01-28 04:03:36 +0800

    Update IXCX_InViewport.js

--- IXCX_InViewport.js
+++ IXCX_InViewport.js
@@ -1,13 +1,14 @@
 class IXCX_InViewport {
     constructor(objectData = { target: null, callback: null }) {
         this.html = document.documentElement;
+        this.body = document.body;
         this.target = objectData.target;
         this.callback = objectData.callback;
     }
 
     start() {
         let isInViewport = () => {
-            if (this.html.scrollTop >= (this.target.offsetTop - (this.target.offsetTop / 2.5))) {
+            if ((this.html.scrollTop || this.body.scrollTop) >= (this.target.offsetTop - (this.target.offsetTop / 2.2))) {
                 this.callback();
                 this.html = 0;
                 this.target = 1;
