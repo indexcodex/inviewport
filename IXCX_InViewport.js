472a5d2f410d419d63a88f5c2ea1316cff02a428
indexcodex <60354991+indexcodex@users.noreply.github.com>
2020-02-05 16:28:18 +0800

    added offsetTrigger

--- IXCX_InViewport.js
+++ IXCX_InViewport.js
@@ -1,14 +1,18 @@
 class IXCX_InViewport {
-    constructor(objectData = { target: null, callback: null }) {
+    constructor(objectData = { target: null, callback: null, offsetTrigger: null,}) {
         this.html = document.documentElement;
         this.body = document.body;
+
+        this.innerHeight_half = window.innerHeight / 2;
+
         this.target = objectData.target;
         this.callback = objectData.callback;
+        this.offsetTrigger = objectData.offsetTrigger;
     }
 
     start() {
         let isInViewport = () => {
-            if ((this.html.scrollTop || this.body.scrollTop) >= (this.target.offsetTop - (this.target.offsetTop / 2.2))) {
+            if ((this.html.scrollTop || this.body.scrollTop) >= (this.target.offsetTop - (this.offsetTrigger == null ? this.innerHeight_half : this.offsetTrigger))) {
                 this.callback();
                 this.html = 0;
                 this.body = 0;
