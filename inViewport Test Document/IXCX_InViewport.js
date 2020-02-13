8d550fa88225f57d8cceb044099e07d51cc226bd
indexcodex <60354991+indexcodex@users.noreply.github.com>
2020-02-14 00:18:29 +0800

    added regex

--- inViewport Test Document/IXCX_InViewport.js
+++ inViewport Test Document/IXCX_InViewport.js
@@ -1,9 +1,20 @@
 class IXCX_InViewport {
-    constructor(objectData = {target: null, callback: null, offsetTrigger: null, debugMode: false}) {
+    constructor(objectData = { target: null, callback: null, offsetTrigger: null, debugMode: false }) {
         this.target = objectData.target;
         this.callback = objectData.callback;
+        this.innerHeight = window.innerHeight;
         this.innerHeight_half = window.innerHeight / 2;
-        this.offsetTrigger = objectData.offsetTrigger;
+        this.offsetTriggerStr = objectData.offsetTrigger;
+
+        if (!objectData.offsetTrigger) {
+            this.offsetTriggerInt = innerHeight;
+        } else if (/\dpx$/.test(this.offsetTriggerStr) == true) {
+            this.offsetTriggerInt = innerHeight - parseInt(objectData.offsetTrigger.replace('px', ''))
+        } else if (/\d%$/.test(this.offsetTriggerStr) == true) {
+            this.offsetTriggerInt = innerHeight * (parseInt(objectData.offsetTrigger.replace('%', '')) / 100)
+        } else {
+            this.offsetTriggerInt = innerHeight;
+        }
 
         // DEBUG
         this.debugMode = objectData.debugMode;
@@ -12,24 +23,24 @@ class IXCX_InViewport {
 
         if (this.debugMode == true || this.debugMode == 1) {
             let body = document.body;
-            this.debugElem.setAttribute('style', 'background: rgba(255, 0, 0, 0.5); width: 100%; height: ' + (this.offsetTrigger ? this.offsetTrigger : this.innerHeight_half) + 'px; position: fixed; top: 0; left: 0; font-family: sans-serif; font-size: 20px; display: flex; justify-content: center; align-items: center;');
+            this.debugElem.setAttribute('style', 'background: rgba(255, 0, 0, 0.5); width: 100%; height: ' + this.offsetTriggerInt + 'px; position: fixed; top: 0; left: 0; font-family: sans-serif; font-size: 20px; display: flex; justify-content: center; align-items: center;');
             body.appendChild(this.debugElem);
         }
     }
     start() {
         let isInViewport = () => {
-            if(this.target) {
+            if (this.target) {
                 let target = this.target.getBoundingClientRect();
                 let targetPosition = target.top;
-                
-                if (targetPosition <= (this.offsetTrigger ? this.offsetTrigger : this.innerHeight_half)) {
+
+                if (targetPosition <= (this.offsetTriggerInt ? this.offsetTriggerInt : this.innerHeight)) {
                     this.callback();
 
                     if (this.debugMode == true || this.debugMode == 1) {
                         console.log('Callback Activated');
                         this.debugElem.appendChild(this.txtNode);
                     }
-                    
+
                     this.target = null;
                     target = null;
                     targetPosition = null;
